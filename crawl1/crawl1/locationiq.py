ce872c1b716a0c2b2b2acc19f999e2b8399ad472
James Nguyen <frankierockie259@gmail.com>
2018-12-03 14:48:18 +0700

    locationiq research

--- crawl1/crawl1/locationiq.py
+++ crawl1/crawl1/locationiq.py
@@ -8,25 +8,27 @@ url = "https://us1.locationiq.com/v1/search.php"
 file =  'test.json'
 fileout = 'out3.json'
 
-# with open(file,'r') as f:
-#     contents = json.load(f)
-#     f.close()
-# for index,content in enumerate(contents):
-#     address = contents[index]['event_place']
-#     # print(address)
-#     data = {
-#         'key': '836f98e04aadb6',
-#         'q': address,
-#         'format': 'json'
-#     }
-#     response = requests.get(url, params=data)
-#     print(response.text)
-
-data = {
-    'key': '836f98e04aadb6',
-    'q': 'Khu vực Vườn hoa Tượng đài Lý Thái Tổ, quận Hoàn Kiếm, Hà Nội',
-    'format': 'json'
-}
-response = requests.get(url, params=data)
-print(response.text)
-#compare output
+try:
+    with open(file,'r') as f:
+        contents = json.load(f)
+        f.close()
+    for index,content in enumerate(contents):
+        address = contents[index]['event_place']
+        data = {
+            'key': '836f98e04aadb6',
+            'q': address,
+            'format': 'json'
+        }
+        response = requests.get(url, params=data)
+        result = response.json()
+        if result and len(result):
+            sys.stderr.write("Found: %s\n" % address)
+            contents[index]["lat"] = result[0]['lat']
+            contents[index]["lng"] = result[0]['lon']
+        else:
+            sys.stderr.write("Not found: %s\n" % address)
+    with codecs.open(fileout,'w',encoding='utf8') as out:
+        json.dump(contents,out,ensure_ascii=False)
+        out.close()
+except IOError:
+    print('Error: File %s does not appear to exist.' % file)
